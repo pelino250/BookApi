d92a14defbfe6b3bd077800eb5823f94e650e0f2
iPelino <pelmut2000@gmail.com>
2025-05-29 12:18:46 +0200

    removed http client env from git

--- books/urls.py
+++ books/urls.py
@@ -6,6 +6,9 @@ from books import views
 # Create a router and register our viewsets with it
 router = DefaultRouter()
 router.register(r'books', views.BookViewSet, basename='book')
+router.register(r'authors', views.AuthorViewSet, basename='author')
+router.register(r'publishers', views.PublisherViewSet, basename='publisher')
+router.register(r'reviews', views.ReviewViewSet, basename='review')
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
